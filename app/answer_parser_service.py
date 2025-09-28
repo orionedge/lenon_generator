fb7b2453c329853839930969fb3e668476139e6c
nyel <yhiamdan@gmail.com>
2025-09-28 16:55:50 +0000

    updates

--- app/answer_parser_service.py
+++ app/answer_parser_service.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from openai import OpenAI
 from app.system_prompts import format_answers
+import os
 load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_APIKEY"))
 
